4c023dca99dab8b5311886b266e76a1cdbb64f73
Flori <florentina@webcrumbz.co>
2014-05-19 16:59:32 +0100

    Debugging

--- wp-content/plugins/wordpress-mobile-pack/export/export-class.php
+++ wp-content/plugins/wordpress-mobile-pack/export/export-class.php
@@ -23,9 +23,9 @@ require_once '../libs/htmlpurifier-4.6.0/library/HTMLPurifier.auto.php';
 		
 		// set HTML Purifier
 		$config = HTMLPurifier_Config::createDefault();
-		$config->set('Core','Encoding', 'UTF-8'); 									
+		$config->set('Core.Encoding', 'UTF-8'); 									
 		$config->set('HTML.Allowed','a[href],p,ol,li,ul,img[src],blockquote,em,span,h1,h2,h3,h4,h5,h6,i,u,strong,b,sup,br,cite,iframe[frameborder|marginheight|marginwidth|scrolling|src|width|height]');
-		$config->set('Attr', 'AllowedFrameTargets', '_blank, _parent, _self, _top');
+		$config->set('Attr.AllowedFrameTargets', '_blank, _parent, _self, _top');
 		$config->set('HTML.ForbiddenElements', 'style,class');
 		
 		$config->set('HTML.SafeIframe',1);
