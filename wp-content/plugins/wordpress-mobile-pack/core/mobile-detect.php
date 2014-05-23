693668d66e164ae64904afb76530294f9b623797
Flori <florentina@webcrumbz.co>
2014-05-23 12:52:34 +0100

    Change detection

--- wp-content/plugins/wordpress-mobile-pack/core/mobile-detect.php
+++ wp-content/plugins/wordpress-mobile-pack/core/mobile-detect.php
@@ -13,12 +13,14 @@ class WPMobileDetect {
 		$load_app = false;
 		
 		require_once (WMP_PLUGIN_PATH.'libs/mobileesp/mdetect.php');
-		$uagent_obj = new uagent_info();
+		$uagentObj = new uagent_info();
 		
-		$is_tablet = $uagent_obj->DetectTierTablet();
-		$is_mobile = $uagent_obj->DetectTierIphone();
+		$is_tablet = $uagentObj->DetectTierTablet();
+		$is_mobile = $uagentObj->DetectTierIphone();
+		$is_webkit = $uagentObj->DetectWebkit();
+		$is_windows_mobile = $uagentObj->DetectWindowsPhone8();
 		
-		if($is_mobile && !$is_tablet) {
+		if($is_mobile && !$is_tablet && ($is_webkit || $is_windows_mobile)) {
 			
 			// set load app cookie	
 			setcookie("wmp_load_app", 1, time()+3600*7*24,'/');
