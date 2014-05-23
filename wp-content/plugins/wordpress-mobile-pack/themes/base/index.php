472efad76c881f809da867e2df5f59dcbf128335
Alexandra <alexandra@webcrumbz.co>
2014-05-23 17:12:43 +0100

    Added google analytics code

--- wp-content/plugins/wordpress-mobile-pack/themes/base/index.php
+++ wp-content/plugins/wordpress-mobile-pack/themes/base/index.php
@@ -101,6 +101,30 @@
 
     <!-- The line below must be kept intact for Sencha Command to build your application -->
     <script id="microloader" type="text/javascript" src=".sencha/app/microloader/development.js"></script>
+    
+    <?php
+        // check if google analytics id was set
+        $google_analytics_id = WMobilePack::wmp_get_setting('google_analytics_id');
+        
+        if ($google_analytics_id != ''):
+    ?>
+    
+        <script type="text/javascript">
+    
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', '<?php echo $google_analytics_id;?>']);
+          _gaq.push(['_trackPageview']);
+        
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        
+        </script>
+        
+    <?php endif;?>
+    
 </head>
 <body>
     <div id="appLoadingIndicator">
