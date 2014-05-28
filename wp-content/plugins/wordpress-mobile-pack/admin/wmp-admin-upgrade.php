5273f60ba32a7e94998c4ac5ea6471ce134c93ec
Flori <florentina@webcrumbz.co>
2014-05-28 16:19:39 +0100

    Upgrade page

--- wp-content/plugins/wordpress-mobile-pack/admin/wmp-admin-upgrade.php
+++ wp-content/plugins/wordpress-mobile-pack/admin/wmp-admin-upgrade.php
@@ -23,20 +23,46 @@
             
             <div class="details">
                 <div class="spacer-10"></div>
-                
-                <h1>MOBILE</h1>
-                <div class="spacer-10"></div>
-                <h1>is more than smartphones </h1>
+                <h1>MOBILE<br>is more than smartphones </h1>
                 <div class="spacer-20"></div>
                 <div class="showcase">
                     <img src="../wp-content/plugins/wordpress-mobile-pack/admin/images/more.png" />
                 </div>
                 <div class="spacer-20"></div>
-                <p></p>
+                <p class="subtitle">Your blog can be an amazing TABLET web application</p>
+                <div class="spacer-20"></div>
+                <div id="" class="try-it">
+                    <a href="javascript:void(0);" id="wmp_waitlist_display_btn" class="btn blue smaller">Try it Now</a>
+                    <div class="spacer-5"></div>
+                    <p>It's free</p>
+                </div>
             </div>
             <div class="spacer-10"></div>
-            <div class="details features">
-            	<div class="spacer-20"></div>
+            <div class="details go-premium">
+            	<div class="spacer-10"></div>
+                <h1>MONETI$E Your Mobile <br />&amp; Tablet Web Application</h1>
+                <div class="spacer-60"></div>
+                <div class="features">
+                	<div class="feature">
+                    	<img src="../wp-content/plugins/wordpress-mobile-pack/admin/images/go-premium-social.png" />
+                        <div class="spacer-5"></div>
+                        <p>Increase social interactions between your readers by giving them the possibility to like, share, tweet &amp; comment directly from within your app.</p>
+                    </div>
+                    <div class="feature">
+                    	<img src="../wp-content/plugins/wordpress-mobile-pack/admin/images/go-premium-payments.png" />
+                        <div class="spacer-5"></div>
+                        <p>Integrate with your favorite Ad Network &amp; Social Micropayments.</p>
+                    </div>
+                    <div class="feature">
+                    	<img src="../wp-content/plugins/wordpress-mobile-pack/admin/images/go-premium-revenue.png" />
+                        <div class="spacer-5"></div>
+                        <p>All the money goes directly in your pockets; there are no shared revenue constraints.</p>
+                    </div>
+                </div>
+                <div class="spacer-60"></div>
+                <div id="" class="try-it">
+                    <a href="javascript:void(0);" id="wmp_waitlist_display_btn" class="btn orange smaller">Go Premium</a>
+                </div>
             </div>
         </div>
         <div class="right-side"> 
