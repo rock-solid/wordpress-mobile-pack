336e9af27f4fc24ade85f2f7beea0a7e601dee4e
Alexandra <alexandra@webcrumbz.co>
2014-06-10 14:53:32 +0100

    Bug fixes

--- wp-content/plugins/wordpress-mobile-pack/wordpress-mobile-pack.php
+++ wp-content/plugins/wordpress-mobile-pack/wordpress-mobile-pack.php
@@ -27,7 +27,7 @@ if ( class_exists( 'WMobilePack' ) && class_exists( 'WMobilePackAdmin' ) ) {
     // Initialize the Wordpress Mobile Pack check logic and rendering
     add_action('plugins_loaded',array( &$wmobile_pack, 'wmp_check_load' ));
         
-	if (is_admin() && current_user_can( 'manage_options' )) {
+	if (is_admin()) {
 		
 		$wmobile_pack->wmp_admin_init();
 		
