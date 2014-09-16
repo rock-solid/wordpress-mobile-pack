d02c352c221cc9ca96515a732b7a253d53376eb5
Flori <florentina@webcrumbz.co>
2014-09-16 14:07:02 +0000

    Safe url

--- wp-content/plugins/wordpress-mobile-pack/themes/base/category.php
+++ wp-content/plugins/wordpress-mobile-pack/themes/base/category.php
@@ -1,9 +1,15 @@
-<?php
+﻿<?php
+require_once(WMP_PLUGIN_PATH.'libs/safestring/safeString.php'); 
 
 $displayed_category = get_the_category();
 
 if ($displayed_category != null && is_array($displayed_category) && count($displayed_category) > 0 && is_object($displayed_category[0])) {
-    header("Location: ".home_url()."/#category/".$displayed_category[0]->cat_name."/".$displayed_category[0]->cat_ID);    
+
+	// strip all special chars
+	$safeString = new safeString();
+	$category_name = $safeString::clearString($displayed_category[0]->cat_name);
+
+	header("Location: ".home_url()."/#category/".$category_name."/".$displayed_category[0]->cat_ID);    
 } else {
     header("Location: ".home_url());
 }
