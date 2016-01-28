3dae1941090d3338e7e51e58e6229a89d52705af
Alexandra <alexandra@webcrumbz.co>
2016-01-28 19:42:00 +0000

    Solve loading bug

--- tests/MiscTest.php
+++ tests/MiscTest.php
@@ -1,32 +1,29 @@
 <?php
 
-if (class_exists('WPMPTestsUtils')) {
+/**
+ * Class MiscTest
+ *
+ * @group integrationTests
+ */
+class MiscTest extends WP_Ajax_UnitTestCase {
 
-    /**
-     * Class MiscTest
-     *
-     * @group integrationTests
-     */
-	class MiscTest extends WP_Ajax_UnitTestCase {
-		
-		protected $old_current_user;
-		
-		function test_feedback_form(){
-			
-			// Become an administrator
-			$this->_setRole( 'administrator' );
-		
-			$_POST['wmp_feedback_page'] = 'Settings';
-			$_POST['wmp_feedback_name'] = 'Test user';
-			$_POST['wmp_feedback_email'] = 'anghel.ac@gmail.com';
-			$_POST['wmp_feedback_message'] = 'Unit testing for feedback form';
-			
-			// Make the request
-			try {
-				$this->_handleAjax( 'wmp_send_feedback' );
-			} catch ( WPAjaxDieContinueException $e ) {
-				unset( $e );
-			}
-		}
-	}
+    protected $old_current_user;
+
+    function test_feedback_form(){
+
+        // Become an administrator
+        $this->_setRole( 'administrator' );
+
+        $_POST['wmp_feedback_page'] = 'Settings';
+        $_POST['wmp_feedback_name'] = 'Test user';
+        $_POST['wmp_feedback_email'] = 'anghel.ac@gmail.com';
+        $_POST['wmp_feedback_message'] = 'Unit testing for feedback form';
+
+        // Make the request
+        try {
+            $this->_handleAjax( 'wmp_send_feedback' );
+        } catch ( WPAjaxDieContinueException $e ) {
+            unset( $e );
+        }
+    }
 }
