1d0a88926b5a9f5d760410a1e8bf5b25693d617f
EkoAlex <alexandru@appticles.com>
2017-08-29 15:35:27 +0300

    Refactor code structure

--- core/class-wmp.php
+++ core/class-wmp.php
@@ -182,24 +182,23 @@ if ( ! class_exists( 'WMobilePack' ) ) {
 		 *
 		 * Display icon reupload notice if icon is uploaded and manifest icon sizes are missing.
 		 *
-		 *
 		 */
 		 public function display_icon_reupload_notice(){
 
 			$icon_filename = WMobilePack_Options::get_setting('icon');
 
-			if ($icon_filename != '' && file_exists(WMP_FILES_UPLOADS_DIR . $icon_filename )) {
+			if ($icon_filename == '') {
+				echo '<div class="notice notice-warning is-dismissible"><p>WP Mobile Pack: Upload an <a href="' . get_admin_url() . 'admin.php?page=wmp-options-theme-settings"/>App Icon</a> to take advantage of the Add To Home Screen functionality!</p></div>';
+
+			} elseif ($icon_filename != '' && file_exists(WMP_FILES_UPLOADS_DIR . $icon_filename)) {
 				foreach (WMobilePack_Uploads::$manifest_sizes as $manifest_size) {
 					if (!file_exists(WMP_FILES_UPLOADS_DIR . $manifest_size . $icon_filename)) {
-						echo '<div class="error"><p>Wordpress Mobile Pack Version 3.1 comes with Add To Home Screen functionality and it requires you to reupload your <a href="' . get_admin_url() . 'admin.php?page=wmp-options-theme-settings"/>App Icon</a>!</p></div>';
+						echo '<div class="notice notice-warning is-dismissible"><p>WP Mobile Pack Version 3.1 comes with Add To Home Screen functionality which requires you to reupload your <a href="' . get_admin_url() . 'admin.php?page=wmp-options-theme-settings"/>App Icon</a>!</p></div>';
 						return;
 					}
 				}
-			} elseif ($icon_filename == '') {
-				echo '<div class="error"><p>Upload an <a href="' . get_admin_url() . 'admin.php?page=wmp-options-theme-settings"/>App Icon</a> to take advantage of the Add To Home Screen functionality!</p></div>';
 			}
-		 }
-
+		}
 
         /**
          *
