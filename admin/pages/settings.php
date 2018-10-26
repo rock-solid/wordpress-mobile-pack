e0cf6a69de855d34b0296b6c3d74bf53b68c55a9
Vaughn Petersen <vpietersen021@gmail.com>
2018-10-26 08:57:54 +0200

    Completion of settings page

--- admin/pages/settings.php
+++ admin/pages/settings.php
@@ -30,6 +30,20 @@ jQuery(document).ready(function() {
 });
 </script>
 
+<style>
+
+.save {
+    background: #9aca40;
+    color: #ffffff;
+    border: 1px solid #7ea82f;
+    border-radius: 3px;
+    padding: 7px 15px 7px 15px;
+    min-width: 120px;
+}
+
+</style>
+
+
 <div id="wmpack-admin">
 	<div class="spacer-60"></div>
     <!-- set title -->
@@ -87,83 +101,29 @@ jQuery(document).ready(function() {
                         <div class="spacer-20"></div>
                         <label>Google Analytics Tracking Code</label>
                         <input class="GATrackingCode" type="text" name="GATrackingCode" value="UA-000000-01" />
-                        <div class="spacer-30"></div>
-                        <div class="submit"><input type="button" id="save" class="save" value="Save Settings"/></div>   
+                        <div class="spacer-20"></div>
+                        <div class="submit"><input type="button" id="save" class="save" value="Save"/></div>   
                          </form>   
-                            <div class="spacer-10"></div>
-
-                            
-                            <div class="spacer-10"></div>
-
-                            
-                            <div class="spacer-10"></div>
-
-                           
-
-                            <div class="spacer-20"></div>
 
                        
+        </div>
                   
                 <div class="spacer-0"></div>
            
-			<div class="spacer-15"></div>
-</form>
+			    <div class="spacer-15"></div>
+
 			<div class="details">
-                
+                <h2 class="title">Enable Social Media Sharing</h2>
                     <div class="spacer-20"></div>
-                    
-                            <div class="spacer-10"></div>
-
-                       
-
-                        <div class="spacer-10"></div>
-                       
-                <div class="spacer-0"></div>
-          
-            <div class="spacer-15"></div>
-            <!--<div class="details">
-                <div class="display-mode">
-                    <h2 class="title">Language Settings</h2>
-                    <div class="spacer-20"></div>
-                    <p>Wordpress Mobile Pack will automatically translate your mobile web app in one of the supported languages: Bosnian, Chinese (zh_CN), Dutch, English, French, German, Hungarian, Italian, Polish, Portuguese (Brazil), Romanian, Spanish or Swedish. This is done based on your Wordpress settings and doesn't require additional changes from the plugin. A big thanks to all of our <a href="https://wordpress.org/plugins/wordpress-mobile-pack/other_notes/" target="_blank">contributors</a>.</p>
+                    <input type="checkbox" name="socialMedia" value="Facebook" checked="true"> Enable Facebook Sharing <br>
                     <div class="spacer-10"></div>
-                    <p>However, if you wish to add another language or change the labels for your current one, you can do so by editing the language files located in <strong><?php #echo WMP_PLUGIN_PATH."frontend/locales";?></strong>. To ensure your translation file will not be overwritten by future updates, please send it to our <a href="mailto:<?php #echo WMP_FEEDBACK_EMAIL;?>">support team</a>.</p>
+                    <input type="checkbox" name="socialMedia" value="Twitter" checked="true"> Enable Twitter Sharing <br>
                     <div class="spacer-10"></div>
-                </div>
-                <div class="spacer-0"></div>
-            </div>-->
+                    <input type="checkbox" name="socialMedia" value="Google+" checked="true"> Enable Google+ Sharing <br>
+                    <div class="spacer-10"></div>
+                    <input type="checkbox" name="socialMedia" value="Whatsapp" checked="true"> Enable WhatsApp Sharing <br>
 
-            <div class="spacer-15"></div>
-
-			<!--<div class="details">
-            	<div class="display-mode">
-                 	<h2 class="title">Tracking</h2>
-                    <div class="spacer-20"></div>
-
-                    <form name="wmp_allowtracking_form" id="wmp_allowtracking_form" class="left" action="<?php #echo admin_url('admin-ajax.php'); ?>?action=wmp_settings_save" method="post">
-                        <?php #$enabled_tracking = WMobilePack_Options::get_setting('allow_tracking'); ?>
-
-                        <input type="hidden" name="wmp_option_allow_tracking" id="wmp_option_allow_tracking" value="<?php #echo $enabled_tracking;?>" />
-                        <input type="checkbox" name="wmp_allowtracking_check" id="wmp_allowtracking_check" value="1" <?php #if ($enabled_tracking == 1) echo "checked" ;?> />
-                        <label for="wmp_allowtracking_check"><strong>Allow tracking of this WordPress install's anonymous data.</strong></label>
-                        <div class="spacer-10"></div>
-
-                        <p style="padding-left: 25px;">To maintain this plugin as best as possible, we need to know what we're dealing with: what kinds of other plugins our users are using, what themes, etc. Please allow us to track that data from your install. It will not track any user details, so your security and privacy are safe with us.</p>
-                        <div class="spacer-10"></div>
-                        <a href="javascript:void(0)" id="wmp_allowtracking_send_btn" class="btn green smaller">Save</a>
-                    </form>
-                </div>
-                 <div class="spacer-0"></div>
-            </div>
-        </div>-->
-
-       
-            <!-- waitlist form -->
-            <?php #include_once(WMP_PLUGIN_PATH.'admin/sections/waitlist.php'); ?>
-
-            <!-- add feedback form -->
-            <?php #include_once(WMP_PLUGIN_PATH.'admin/sections/feedback.php'); ?>
-        </div>
+      </form>              
 	</div>
 </div>
 </div>
